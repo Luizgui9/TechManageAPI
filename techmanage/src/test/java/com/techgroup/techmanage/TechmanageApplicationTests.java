a5073f710082bfd57daeb1308e1e66e2364f09fa
Luizgui9 <lg.novaes@yahoo.com.br>
2025-02-05 13:09:51 -0300

    Projeto inicial SpringBoot TechManage
    Commit com configurações iniciais do projeto TechManageAPI, tais como itens do SpringBoot e configuração inicial do application.properties para funcionamento com base de dados H2.

---
+++ techmanage/src/test/java/com/techgroup/techmanage/TechmanageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techgroup.techmanage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechmanageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
