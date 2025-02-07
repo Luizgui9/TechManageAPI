253632260ff505b5ce974862fd87950ff24f384b
Luizgui9 <lg.novaes@yahoo.com.br>
2025-02-06 22:17:19 -0300

    Criação data.sql e ajuste application.properties
    - Criação de arquivo data.sql contendo valores iniciais para o banco de dados H2 ao
     inicializar o Spring;
    - Ajuste application.properties para criação de tabelas automáticas por JPA ocorrerem antes
     da tentativa de inserção de dados iniciais pelo Spring;

---
+++ techmanage/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO users (full_name, email, phone, birth_date, user_type)
+VALUES
+('Alice Johnson', 'alice@example.com', '1234567890', '1990-05-15', 'ADMIN'),
+('Bob Smith', 'bob@example.com', '9876543210', '1985-10-22', 'VIEWER'),
+('Ruan', 'ru_an@example.com', '9777643210', '2000-11-05', 'EDITOR');
